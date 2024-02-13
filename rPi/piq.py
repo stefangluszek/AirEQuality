a678d5e70f9856c5dd39c6fe380b6dde840e4018
Stefan Gluszek <stefang@makerslink.se>
2024-02-13 06:45:56 +0000

    Added the rPi code.

---
+++ rPi/piq.py
@@ -0,0 +1,104 @@
+import RPi.GPIO as GPIO
+import serial
+import struct
+from time import sleep, time
+from collections import deque
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+print(f"Running on {GPIO.RPI_INFO}!")
+
+# Setup channels
+SET = 23
+RESET = 24
+TX = 14
+RX = 15
+
+HISTORY = 1440
+INTERVAL = 60
+
+START1 = b'\x42'
+START2 = b'\x4d'
+LOG = "log.csv"
+
+pm10 = deque(maxlen=HISTORY)
+pm25 = deque(maxlen=HISTORY)
+pm100 = deque(maxlen=HISTORY)
+part03 = deque(maxlen=HISTORY)
+part05 = deque(maxlen=HISTORY)
+part10 = deque(maxlen=HISTORY)
+part25 = deque(maxlen=HISTORY)
+part50 = deque(maxlen=HISTORY)
+part100 = deque(maxlen=HISTORY)
+
+
+GPIO.setup([SET,RESET], GPIO.OUT, initial=GPIO.HIGH)
+
+serial = serial.Serial("/dev/serial0", 9600)
+f = open(LOG, "a")
+
+while True:
+	# Wait for the start characters
+	while b1 := serial.read() != START1:
+		continue
+
+	if b2:= serial.read() != START2:
+		continue
+
+
+	b = serial.read(30)
+	(
+		lenght,
+		pm10_standard,
+		pm25_standard,
+		pm100_standard,
+		pm10_env,
+		pm25_env,
+		pm_100_env,
+		part_03um,
+		part_05um,
+		part_10um,
+		part_25um,
+		part_50um,
+		part_100um,
+		reserved,
+		checksum
+	) = struct.unpack(">15H", b)
+
+	print("####### AIR QUALITY #############")
+	print(f"PM1.0: {pm10_standard}uq/m3")
+	print(f"PM2.5: {pm25_standard}uq/m3")
+	print(f"PM10: {pm100_standard}uq/m3")
+	print(f"particles over 0.3um: {part_03um} in 0.1L of air")
+	print(f"particles over 0.5um: {part_05um} in 0.1L of air")
+	print(f"particles over 1.0um: {part_10um} in 0.1L of air")
+	print(f"particles over 2.5um: {part_25um} in 0.1L of air")
+	print(f"particles over 5.0um: {part_50um} in 0.1L of air")
+	print(f"particles over 10.0um: {part_100um} in 0.1L of air")
+	print()
+
+	fields = [
+		int(time()),
+		lenght,
+		pm10_standard,
+		pm25_standard,
+		pm100_standard,
+		pm10_env,
+		pm25_env,
+		pm_100_env,
+		part_03um,
+		part_05um,
+		part_10um,
+		part_25um,
+		part_50um,
+		part_100um,
+		reserved,
+		checksum
+	]
+
+	line = ",".join(str(f) for f in fields)
+
+	f.write(line + "\n")
+
+	sleep(INTERVAL)
