184603dfe38ed19af4d4f618aa79bacb6501ce7d
Stefan Gluszek <stefang@kahoot.com>
2024-02-17 08:59:39 +0100

    Plot the values over time.

--- rPi/piq.py
+++ rPi/piq.py
@@ -1,8 +1,10 @@
+import struct
+from collections import deque
+from time import sleep, time
+
+import matplotlib.pyplot as plt
 import RPi.GPIO as GPIO
 import serial
-import struct
-from time import sleep, time
-from collections import deque
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -18,8 +20,8 @@ RX = 15
 HISTORY = 1440
 INTERVAL = 60
 
-START1 = b'\x42'
-START2 = b'\x4d'
+START1 = b"\x42"
+START2 = b"\x4d"
 LOG = "log.csv"
 
 pm10 = deque(maxlen=HISTORY)
@@ -33,72 +35,97 @@ part50 = deque(maxlen=HISTORY)
 part100 = deque(maxlen=HISTORY)
 
 
-GPIO.setup([SET,RESET], GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup([SET, RESET], GPIO.OUT, initial=GPIO.HIGH)
 
 serial = serial.Serial("/dev/serial0", 9600)
 f = open(LOG, "a")
 
+fig, ax = plt.subplots()
+
 while True:
-	# Wait for the start characters
-	while b1 := serial.read() != START1:
-		continue
+    # Wait for the start characters
+    while b1 := serial.read() != START1:
+        continue
 
-	if b2:= serial.read() != START2:
-		continue
+    if b2 := serial.read() != START2:
+        continue
 
+    b = serial.read(30)
+    (
+        lenght,
+        pm10_standard,
+        pm25_standard,
+        pm100_standard,
+        pm10_env,
+        pm25_env,
+        pm_100_env,
+        part_03um,
+        part_05um,
+        part_10um,
+        part_25um,
+        part_50um,
+        part_100um,
+        reserved,
+        checksum,
+    ) = struct.unpack(">15H", b)
 
-	b = serial.read(30)
-	(
-		lenght,
-		pm10_standard,
-		pm25_standard,
-		pm100_standard,
-		pm10_env,
-		pm25_env,
-		pm_100_env,
-		part_03um,
-		part_05um,
-		part_10um,
-		part_25um,
-		part_50um,
-		part_100um,
-		reserved,
-		checksum
-	) = struct.unpack(">15H", b)
+    pm10.append(pm10_standard)
+    pm25.append(pm25_standard)
+    pm100.append(pm100_standard)
+    part03.append(part_03um)
+    part05.append(part_05um)
+    part10.append(part_10um)
+    part25.append(part_25um)
+    part50.append(part_50um)
+    part100.append(part_100um)
 
-	print("####### AIR QUALITY #############")
-	print(f"PM1.0: {pm10_standard}uq/m3")
-	print(f"PM2.5: {pm25_standard}uq/m3")
-	print(f"PM10: {pm100_standard}uq/m3")
-	print(f"particles over 0.3um: {part_03um} in 0.1L of air")
-	print(f"particles over 0.5um: {part_05um} in 0.1L of air")
-	print(f"particles over 1.0um: {part_10um} in 0.1L of air")
-	print(f"particles over 2.5um: {part_25um} in 0.1L of air")
-	print(f"particles over 5.0um: {part_50um} in 0.1L of air")
-	print(f"particles over 10.0um: {part_100um} in 0.1L of air")
-	print()
+    ax.clear()
+    ax.plot(pm10, label="PM10")
+    ax.plot(pm25, label="PM2.5")
+    ax.plot(pm100, label="PM100")
+    ax.plot(part03, label="Particles > 0.3um")
+    ax.plot(part05, label="Particles > 0.5um")
+    ax.plot(part10, label="Particles > 1.0um")
+    ax.plot(part25, label="Particles > 2.5um")
+    ax.plot(part50, label="Particles > 5.0um")
+    ax.plot(part100, label="Particles > 10.0um")
+    ax.legend()
 
-	fields = [
-		int(time()),
-		lenght,
-		pm10_standard,
-		pm25_standard,
-		pm100_standard,
-		pm10_env,
-		pm25_env,
-		pm_100_env,
-		part_03um,
-		part_05um,
-		part_10um,
-		part_25um,
-		part_50um,
-		part_100um,
-		reserved,
-		checksum
-	]
+    plt.pause(INTERVAL)
 
-	line = ",".join(str(f) for f in fields)
+    print("####### AIR QUALITY #############")
+    print(f"PM1.0: {pm10_standard}uq/m3")
+    print(f"PM2.5: {pm25_standard}uq/m3")
+    print(f"PM10: {pm100_standard}uq/m3")
+    print(f"particles over 0.3um: {part_03um} in 0.1L of air")
+    print(f"particles over 0.5um: {part_05um} in 0.1L of air")
+    print(f"particles over 1.0um: {part_10um} in 0.1L of air")
+    print(f"particles over 2.5um: {part_25um} in 0.1L of air")
+    print(f"particles over 5.0um: {part_50um} in 0.1L of air")
+    print(f"particles over 10.0um: {part_100um} in 0.1L of air")
+    print()
 
-	f.write(line + "\n")
+    fields = [
+        int(time()),
+        lenght,
+        pm10_standard,
+        pm25_standard,
+        pm100_standard,
+        pm10_env,
+        pm25_env,
+        pm_100_env,
+        part_03um,
+        part_05um,
+        part_10um,
+        part_25um,
+        part_50um,
+        part_100um,
+        reserved,
+        checksum,
+    ]
 
-	sleep(INTERVAL)
+    line = ",".join(str(f) for f in fields)
+
+    f.write(line + "\n")
+
+    sleep(INTERVAL)
